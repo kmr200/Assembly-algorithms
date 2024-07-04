dac460cdfb3913544ced2d9fcd43acc5d1f2e4a1
Kamron Shuhratjonov <125473962+kmr200@users.noreply.github.com>
2024-07-04 08:52:01 +0500

    Update Fibonacci.s
    Edited the program for it be called from C

--- Fibonacci.s
+++ Fibonacci.s
@@ -1,57 +1,41 @@
-.data
-@ n'th element from Fibonaci sequence
-n: .word 10
-result: .word 0
+.global fib
+fib:
+    @ Input: r0 = n
+    mov r3, #0 @ n-2 element of sequence
+    mov r4, #1 @ n-1 element of sequence
 
-.global _start
-_start:
+    cmp r0, #0
+    beq return_0
 
-	mov r3, #0 @ n-2 element of sequence
-	mov r4, #1 @ n-1 element of sequence
-	
+    cmp r0, #1
+    beq return_1
 
-	ldr r0, =n @ Pointer to 'n'
-	ldr r1, [r0] @ Get the value of 'n'
-	
-	@ Return 0 if n == 0
-	cmp r1, #0
-	beq return_0
-	
-	@ Return 1 if n == 1
-	cmp r1, #1
-	beq return_1
-	
-	mov r2, #1 @ Counter variable
-	mov r5, #1 @ Prepare summing register
-	
-	b sequence_loop
-	
+    mov r2, #1 @ Counter variable
+    mov r5, #1 @ Prepare summing register
+
+    b sequence_loop
+
 return_0:
-	
-	mov r5, #0
-	b exit
-	
-return_1:
-	
-	mov r5, #1
-	b exit
-	
-sequence_loop:
+    mov r0, #0
+    bx lr
 
-	cmp r2, r1
-	
-	beq exit
-	
-	add r5, r3, r4 @ An = An-2 + An-1
-	@ Prepare values for the next iteration
-	mov r3, r4
-	mov r4, r5
-	
-	add r2, #1
-	
-	b sequence_loop @ Branch back
-	
+return_1:
+    mov r0, #1
+    bx lr
+
+sequence_loop:
+    cmp r2, r0
+    beq exit
+
+    add r5, r3, r4 @ An = An-2 + An-1
+    @ Prepare values for the next iteration
+    mov r3, r4
+    mov r4, r5
+
+    add r2, r2, #1
+
+    b sequence_loop @ Branch back
+
 exit:
-	ldr r6, =result @ Pointer to memory location 'result'
-	str r5, [r6]
-	
+    mov r0, r5 @ Store result in r0
+    bx lr @ Return from function
