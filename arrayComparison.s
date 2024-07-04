697cd2f52f8f099a3368e635cf57d2a3d8492d84
Kamron Shuhratjonov <125473962+kmr200@users.noreply.github.com>
2024-07-04 08:53:17 +0500

    Update arrayComparison.s
    Edited the program for it to be called from C

--- arrayComparison.s
+++ arrayComparison.s
@@ -1,70 +1,62 @@
-.data
-array1: .word 1, 2, 3, 4, 5, 6, 7, 8, 9
-array2: .word 2, 3, 4, 5, 6, 7, 8, 9, 10
-result: .word 0
+.global compare_arrays
+compare_arrays:
+    @ Input: r0 = array1, r1 = array2, r2 = length
+    mov r3, r0 @ Array1 pointer
+    mov r4, r1 @ Array2 pointer
+    mov r2, #9 @ Length of the arrays
 
-.global _start
-_start:
-	mov r0, #0 @Prepare registre to write result
-	mov r1, #0 @Counter
-	mov r2, #4 @Offset
-	ldr r3, =array1 @Array1 pointer
-	ldr r4, =array2 @Array2 pointer
-	ldr r8, =result
-	
+    mov r5, #0 @ Prepare register to write result
+    mov r6, #0 @ Counter
+    mov r7, #4 @ Offset
+
 loop_greater:
-	cmp r1, #9
-	
-	beq loop_greater_exit @End of the loop
+    cmp r6, r2
+    beq loop_greater_exit @ End of the loop
 
-	mul r5, r1, r2 @Array index to be compared
-	
-	ldr r6, [r3, r5] @Array1 value to be compared
-	ldr r7, [r4, r5] @Array2 value to be compared
-	
-	mov r0, #0
-	
-	cmp r6, r7
-	
-	ble loop_greater_exit @exit if value1 <= value2
-	
-	mov r0, #1
-	add r1, #1
-	b loop_greater
-	
+    mul r8, r6, r7 @ Array index to be compared
+
+    ldr r9, [r3, r8] @ Array1 value to be compared
+    ldr r10, [r4, r8] @ Array2 value to be compared
+
+    mov r5, #0
+
+    cmp r9, r10
+    ble loop_greater_exit @ Exit if value1 <= value2
+
+    mov r5, #1
+    add r6, r6, #1
+    b loop_greater
+
 loop_greater_exit:
-	cmp r0, #1
-	
-	beq exit @Branch to exit if loop_greater succeeded
-	
-	@Prepare temp registers:
-	mov r1, #0
-	
-loop_less:
-	cmp r1, #9
-	
-	beq loop_less_exit @End of the loop
+    cmp r5, #1
+    beq exit @ Branch to exit if loop_greater succeeded
 
-	mul r5, r1, r2 @Array index to be compared
-	
-	ldr r6, [r3, r5] @Array1 value to be compared
-	ldr r7, [r4, r5] @Array2 value to be compared
-	
-	cmp r7, r6
-	
-	ble loop_less_exit @exit if value1 >= value2
-	
-	mov r0, #-1
-	add r1, #1
-	
-	b loop_less
-	
+    @ Prepare temp registers:
+    mov r6, #0
+
+loop_less:
+    cmp r6, r2
+    beq loop_less_exit @ End of the loop
+
+    mul r8, r6, r7 @ Array index to be compared
+
+    ldr r9, [r3, r8] @ Array1 value to be compared
+    ldr r10, [r4, r8] @ Array2 value to be compared
+
+    cmp r10, r9
+    ble loop_less_exit @ Exit if value1 >= value2
+
+    mov r5, #-1
+    add r6, r6, #1
+
+    b loop_less
+
 loop_less_exit:
-	cmp r0, #-1
-	
-	beq exit @Branch to exit if loop_less succeeded
-	
-	mov r0, #0 @load 0 to r0 since none of the conditions passed
+    cmp r5, #-1
+    beq exit @ Branch to exit if loop_less succeeded
+
+    mov r5, #0 @ Load 0 to r5 since none of the conditions passed
 
 exit:
-	str r0, [r8]
+    mov r0, r5 @ Store result in r0
+    bx lr @ Return from function
